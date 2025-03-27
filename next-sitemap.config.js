cff063bce1e52aafe1bee9899269980f01777950
Andrii Liakh <las.forall@gmail.com>
2025-03-27 12:54:59 +0200

    Оптимизация SEO и создание sitemap.xml для гугла

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://kazantseva-rehab.vercel.app',
+    generateRobotsTxt: true,
+    generateIndexSitemap: false,
+};
